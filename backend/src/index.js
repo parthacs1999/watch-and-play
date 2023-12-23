c9dd4f71d46d0c991cc454ef5aeee2e7386367e1
parthacs1999 <partha.c1999@gmail.com>
2023-12-23 09:14:53 +0530

    Added utils for api response and errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port:- ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed");
+  });
