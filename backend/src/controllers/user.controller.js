d6f9c0807e0963199b9b01a8d6841e2a739bf381
parthacs1999 <partha.c1999@gmail.com>
2024-01-04 08:09:22 +0530

    Fixed bugs

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -11,14 +11,22 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(404, "All fields are required");
   }
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ userName }, { email }],
   });
   if (existedUser) {
     throw new ApiError(404, "User with email or userName already exists");
   }
-  const avatarLocalPath = req.files?.avater[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage[0].length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(404, "Avatar image is not present");
